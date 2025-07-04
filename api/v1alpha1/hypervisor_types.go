1a5f619210e977eb2a5521be48334d43fa3875ca
Fabian Wiesel <fabian.wiesel@sap.com>
2025-07-04 15:48:00 +0200

    Disable certificate generation
    For now leave the functionality in, but disable by default
    so that the hypervisor operator can take over that functionality.

--- api/v1alpha1/hypervisor_types.go
+++ api/v1alpha1/hypervisor_types.go
@@ -40,7 +40,7 @@ type HypervisorSpec struct {
 	// EvacuateOnReboot request an evacuation of all instances before reboot.
 	EvacuateOnReboot bool `json:"evacuateOnReboot"`
 
-	// +kubebuilder:default:=true
+	// +kubebuilder:default:=false
 	// Require to issue a certificate from cert-manager for the hypervisor, to be used for
 	// secure communication with the libvirt API.
 	CreateCertManagerCertificate bool `json:"createCertManagerCertificate"`
